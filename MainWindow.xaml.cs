5620a23eca0887c760d7143167c27bc91ee6827a
abraham743 <133922900+abraham743@users.noreply.github.com>
2023-06-21 17:42:56 -0500

    asads

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,6 +38,9 @@ namespace calculadora6
                 {
                     HandleNumbers(value);
                 }
+                else if (IsOperator(value)) { HandleOperator(value); }
+                else if (value=="CE") { Screen.Clear(); }
+                else if (value =="=") { HandleEquals(Screen.Text); }
 
             }
             catch (Exception ex)
@@ -72,15 +75,107 @@ namespace calculadora6
             }
         }
 
+        private bool IsOperator(string possibleOperator)
+        {
+            //if(possibleOperator =="+" || possibleOperator =="-" || possibleOperator=="*" ||
+               // possibleOperator=="/") 
+            //{
+                return possibleOperator == "+" || possibleOperator == "-" || possibleOperator == "*" ||
+                possibleOperator=="/";
+            //}
+        }
 
+        private void HandleOperator(string value) 
+        { 
+            if(!String.IsNullOrEmpty(Screen.Text)&& !ContainsOtherOperator(Screen.Text))
+            {
+                Screen.Text += value;
+            }
+        }
+        
+        //validacion
+        
+        private bool ContainsOtherOperator(string screenContent)
+        {
+            return screenContent.Contains("+") || screenContent.Contains("-") 
+                || screenContent.Contains("*") || screenContent.Contains("/");
+        }
+        
+        private void HandleEquals(string ScreenContenet)
+        {
+            string op=FindOperator(ScreenContenet);
+            if(!string.IsNullOrEmpty(op))
+            {
+                switch (op)
+                {
+                    case "+":
+                        Screen.Text = Sum();
+                        break;
 
+                        case "-":
+                        Screen.Text = Res();
+                       break;
 
+                    case "*":
+                        Screen.Text = Mult();
+                        break;
 
+                        case "/":
+                        Screen.Text = Div();
+                        break;
+                    default:
+                        break;
 
+                }
+            }
+        }
 
+        private string FindOperator(string screenContenet)
+        {
+            foreach (var c in screenContenet)
+            {
+                if (IsOperator(c.ToString()))
+                { return c.ToString(); }
+                
+            }
+            return "";
+        }
+         
+        private string Sum()
+        {
+            string[] number = Screen.Text.Split('+');
+            double.TryParse(number[0], out double n1);
+            double.TryParse(number[1], out double n2);
 
+            return Math.Round(n1 + n2,12).ToString();
+        }
+        private string Res()
+        {
+            string[]number = Screen.Text.Split("-");
+            double.TryParse(number[0], out double n1);
+            double.TryParse(number[1],out double n2);
 
+            return Math.Round(n1 - n2,12).ToString();
 
+        }
+
+        private string Mult()
+        {
+            string[]number=Screen.Text.Split("*");
+            double.TryParse(number[0], out double n1);
+            double.TryParse(number[1], out double n2);
+
+            return Math.Round(n1 * n2,12).ToString();
+        }
+
+        private string Div()
+        {
+            string[] number = Screen.Text.Split("/");
+            double.TryParse(number[0], out double n1);
+            double.TryParse(number[1], out double n2);
+
+            return Math.Round(n1 / n2,12).ToString();
+        }
     }
     
 }
